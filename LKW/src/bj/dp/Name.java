ef6a18c9ade76c69e604dcb2f70c237b360ed000
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-03-15 13:59:56 +0900

    [BOJ]1주차

--- LKW/src/bj/dp/Name.java
+++ LKW/src/bj/dp/Name.java
@@ -29,7 +29,7 @@ public class Name {
             for (int x = 0; x < tp.size(); x++)
                 result.add(tp.get(x));
             tp.clear();
-            if (result.size() == 2) {
+            if (result.size() ==2) {
                 System.out.println(result.get(0) + "" + result.get(1));
                 return;
             }
